8df06bca17daf53dc4a68058053b704b0a8aa353
Su Su <s@warmsea.net>
2021-03-07 10:25:33 +0800

    Clean up Marker

--- src/components/marker/Marker.tsx
+++ src/components/marker/Marker.tsx
@@ -1,12 +1,6 @@
-import React, { FC } from "react";
+import React, { FC, SVGAttributes } from "react";
 
-export interface IMarkerProps {
-  id?: string;
-  refX?: string | number;
-  markerWidth?: string | number;
-  markerHeight?: string | number;
-  fill?: string;
-}
+export type IMarkerProps = SVGAttributes<SVGMarkerElement>;
 
 /**
  * Market component provides configurable interface to marker definition.
@@ -18,14 +12,10 @@ export const Marker: FC<IMarkerProps> = (props: IMarkerProps) => {
   return (
     <marker
       className="marker"
-      id={props.id}
       viewBox="0 -5 10 10"
-      refX={props.refX}
       refY="0"
-      markerWidth={props.markerWidth}
-      markerHeight={props.markerHeight}
       orient="auto"
-      fill={props.fill}
+      {...props}
     >
       <path d="M0,-5L10,0L0,5" />
     </marker>
