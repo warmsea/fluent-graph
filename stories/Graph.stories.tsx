34168d2b5bf736487d61422d609e327750b181c5
Su Su <s@warmsea.net>
2021-03-22 20:31:58 +0800

    Two more examples

--- stories/Graph.stories.tsx
+++ stories/Graph.stories.tsx
@@ -59,3 +59,75 @@ Styled.args = {
     }
   }
 };
+
+export const Loop: Story<ITemplateArgs> = Template.bind({});
+Loop.args = {
+  graphProps: {
+    id: "graph",
+    nodes: [{ id: "A" }, { id: "B1" }, { id: "B2" }, { id: "C" }],
+    links: [
+      { source: "A", target: "B1" },
+      { source: "A", target: "B2" },
+      { source: "B1", target: "C" },
+      { source: "B2", target: "C" },
+    ]
+  }
+};
+
+export const Complex: Story<ITemplateArgs> = Template.bind({});
+Complex.args = {
+  graphProps: {
+    id: "graph",
+    nodes: [
+      { id: "Mark 8", label: "Mark 8" },
+
+      { id: "Aerial", label: "Aerial" },
+      { id: "GIS Mapping" },
+      { id: "Survey Intelligence" },
+      { id: "Flight Operations" },
+
+      { id: "Structual Integrity", label: "Structual Integrity" },
+      { id: "Spaces", label: "Spaces" },
+      { id: "Digital Surfaces" },
+      { id: "3D Modeling" },
+      { id: "Partners" },
+      { id: "Geothermal" },
+      { id: "United" },
+      { id: "Delta" },
+
+      { id: "Swiftly" },
+      { id: "Data Visualization", label: "Data Visualization" },
+      { id: "Fjord" },
+      { id: "Altas" },
+      { id: "Construction Operations Reliability" },
+      { id: "Athena" },
+      { id: "Elements" },
+      { id: "Juno" },
+    ],
+    links: [
+      { source: "Mark 8", target: "Aerial" },
+      { source: "Mark 8", target: "GIS Mapping" },
+      { source: "Mark 8", target: "Survey Intelligence" },
+      { source: "Mark 8", target: "Flight Operations" },
+
+      { source: "Aerial", target: "Structual Integrity" },
+      { source: "Aerial", target: "Spaces" },
+      { source: "GIS Mapping", target: "Digital Surfaces" },
+      { source: "GIS Mapping", target: "3D Modeling" },
+      { source: "Survey Intelligence", target: "Partners" },
+      { source: "Survey Intelligence", target: "Geothermal" },
+      { source: "Flight Operations", target: "United" },
+      { source: "Flight Operations", target: "Delta" },
+
+      { source: "Structual Integrity", target: "Swiftly" },
+      { source: "Structual Integrity", target: "Data Visualization" },
+      { source: "Spaces", target: "Data Visualization" },
+      { source: "Spaces", target: "Fjord" },
+      { source: "Spaces", target: "Altas" },
+      { source: "Spaces", target: "Construction Operations Reliability" },
+      { source: "Partners", target: "Athena" },
+      { source: "United", target: "Elements" },
+      { source: "Delta", target: "Juno" },
+    ],
+  }
+};
