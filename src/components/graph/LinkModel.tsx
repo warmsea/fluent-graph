f4070367d7e889e94186de9fe653fbf97d598fd3
YixuanDong <yixuandd@gmail.com>
2021-03-26 14:10:24 +0800

    add link node to avoid edge overlap (#6)
    Co-authored-by: Yixuan Dong <yido@microsoft.com>

--- src/components/graph/LinkModel.tsx
+++ src/components/graph/LinkModel.tsx
@@ -6,12 +6,16 @@ import { IGraphPropsLink } from "./Graph.types";
 import { NodeMap } from "./NodeMap";
 import { NodeModel } from "./NodeModel";
 import { SimulationLinkDatum, SimulationNodeDatum } from "d3";
+import { default as CONST } from "./graph.const";
+import { Node } from "../node/Node";
 
 export class LinkModel {
   public sourceNode: NodeModel;
   public targetNode: NodeModel;
+  public linkNode: NodeModel;
 
   private id: string;
+  private linkNodeId: string;
   private props: IGraphPropsLink;
 
   public force: SimulationLinkDatum<SimulationNodeDatum>;
@@ -22,9 +26,10 @@ export class LinkModel {
     nodeMap: NodeMap
   ) {
     this.id = getLinkId(props);
-
+    this.linkNodeId = getLinkNodeId(props);
     this.sourceNode = nodeMap.get(props.source);
     this.targetNode = nodeMap.get(props.target);
+    this.linkNode = nodeMap.get(this.linkNodeId);
     this.props = mergeConfig(linkConfig, props);
     this.force = {
       source: this.props.source,
@@ -61,8 +66,26 @@ export class LinkModel {
       />
     );
   }
+
+  public renderLinkNode(): JSX.Element {
+    return (
+      <g
+        key={this.linkNodeId}
+        transform={`translate(${this.linkNode.force.x ?? 0}, ${this.linkNode.force.y ?? 0})`}
+      >
+        <Node
+          size={0} // TODO: polish the implement to hide the node.
+          id={this.linkNodeId}
+        />
+      </g >
+    );
+  }
 }
 
 export function getLinkId(link: { source: string; target: string }): string {
   return `${link.source},${link.target}`;
+}
+
+export function getLinkNodeId(link: { source: string; target: string }): string {
+  return `${CONST.LINK_NODE_PREFIX}${link.source}-${link.target}`;
 }
