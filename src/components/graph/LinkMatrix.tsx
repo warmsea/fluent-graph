a79c8f1a3a1ce83d3f445827e08d6efcb9f5b5d7
Su Su <s@warmsea.net>
2021-07-10 21:33:20 +0800

    Build a Matirx util

--- src/components/graph/LinkMatrix.tsx
+++ src/components/graph/LinkMatrix.tsx
@@ -1,13 +1,14 @@
+import { Matrix } from "../../Matrix";
 import { IGraphPropsLink } from "./Graph.types";
 import { LinkMap } from "./LinkMap";
 import { getLinkId, LinkModel } from "./LinkModel";
 import { NodeMap } from "./NodeMap";
 
 export class LinkMatrix {
-  private _matrix: Map<string, Map<string, LinkModel>>;
+  private _matrix: Matrix<string, LinkModel>;
 
   constructor(links: IGraphPropsLink[], linkMap: LinkMap, nodeMap: NodeMap) {
-    this._matrix = new Map();
+    this._matrix = new Matrix();
 
     // Rely on the order input by the consumer
     links.forEach((link: IGraphPropsLink) => {
@@ -18,18 +19,12 @@ export class LinkMatrix {
         return;
       }
 
-      if (!this._matrix.has(source)) {
-        this._matrix.set(source, new Map());
-      }
-      if (!this._matrix.has(target)) {
-        this._matrix.set(target, new Map());
-      }
+      // The later link wins on duplicate
+      this._matrix.set(source, target, linkMap.get(getLinkId(link)));
 
-      // The later one wins on duplicate
-      this._matrix.get(source)!.set(target, linkMap.get(getLinkId(link)));
-
-      if (!this._matrix.get(target)!.has(source)) {
-        this._matrix.get(target)!.set(source, linkMap.get(getLinkId(link)));
+      // Unless explicit specified, links are di-directed.
+      if (!this._matrix.get(target, source)) {
+        this._matrix.set(target, source, linkMap.get(getLinkId(link)));
       }
     });
   }
@@ -38,6 +33,6 @@ export class LinkMatrix {
     sourceId: string,
     callback: (link: LinkModel) => void
   ) {
-    this._matrix.get(sourceId)?.forEach(callback);
+    this._matrix.getRow(sourceId).forEach(callback);
   }
 }
---
+++ test/Matrix.test.ts
@@ -0,0 +1,32 @@
+import { Matrix } from "../src/Matrix";
+
+describe("Matrix", () => {
+  describe("get", () => {
+    it("should return undefined if the value does not exist", () => {
+      const matrix = new Matrix<string, string>();
+      expect(matrix.get("a", "b")).toBeUndefined();
+    });
+
+    it("should return the correct value", () => {
+      const matrix = new Matrix<string, string>();
+      matrix.set("a", "b", "hello");
+      matrix.set("a", "c", "world");
+      expect(matrix.get("a", "b")).toBe("hello");
+      expect(matrix.get("a", "c")).toBe("world");
+    });
+  });
+
+  describe("getRow", () => {
+    it("should return an empty array if the row does not exist", () => {
+      const matrix = new Matrix<string, string>();
+      expect(matrix.getRow("a")).toEqual([]);
+    });
+
+    it("should return an empty array if the row does not exist", () => {
+      const matrix = new Matrix<string, string>();
+      matrix.set("a", "b", "hello");
+      matrix.set("a", "c", "world");
+      expect(matrix.getRow("a")).toEqual(["hello", "world"]);
+    });
+  });
+});
