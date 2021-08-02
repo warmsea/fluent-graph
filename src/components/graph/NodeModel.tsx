86d8605736dd202b7dfc0438487e1bfd2cbc267f
Yixuan Dong <yido@microsoft.com>
2021-08-02 15:56:00 +0800

    fix the bug that fx and fy does not get override

--- src/components/graph/NodeModel.tsx
+++ src/components/graph/NodeModel.tsx
@@ -50,6 +50,8 @@ export class NodeModel {
     this.size = this.props.size ?? 0;
     this.relatedNodesOfLinkNode = relatedNodesOfLinkNode ?? [];
     this.isLinkNode = !!isLinkNode;
+    this.force.fx = props.force?.fx;
+    this.force.fy = props.force?.fy;
   }
 
   public renderNode(): JSX.Element {
