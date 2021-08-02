20715187ceaf5db5bec4b3b9c6daf5862f2d632c
Yixuan Dong <yido@microsoft.com>
2021-08-02 16:29:17 +0800

    remove unused interface

--- src/components/graph/Graph.types.ts
+++ src/components/graph/Graph.types.ts
@@ -34,7 +34,6 @@ export interface IGraphProps {
   id: string;
   nodes: IGraphPropsNode[];
   links: IGraphPropsLink[];
-  focusedNodeId?: string;
   nodeConfig?: INodeCommonConfig;
   linkConfig?: ILinkCommonConfig;
   config?: IGraphPropsConfig;
