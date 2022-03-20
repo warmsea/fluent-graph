4bf720336aad15820db9d51daf7d913eb0f362c8
Su Su <s@warmsea.net>
2022-03-20 11:09:34 +0800

    Simplify and upgrade dependencies

--- stories/graph/Graph.stories.tsx
+++ stories/graph/Graph.stories.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Meta, Story } from "@storybook/react";
 import { Graph } from "../../src";
-import { Args, BaseStory } from "@storybook/addons";
-import { StoryFnReactReturnType } from "@storybook/react/dist/client/preview/types";
 import { IGraphProps } from "../../src/components/graph/Graph.types";
 
 const meta: Meta = {
@@ -11,7 +9,7 @@ const meta: Meta = {
 
 export default meta;
 
-interface ITemplateArgs extends BaseStory<Args, StoryFnReactReturnType> {
+interface ITemplateArgs {
   graphProps: IGraphProps;
 }
 
